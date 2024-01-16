7bcf178092bcd42d00a6e3ae85a7ef5c09430ae7
jeon yul <jeonyul.citylabs@gmail.com>
2024-01-16 11:59:45 +0900

    feat: lottie

--- coupang/SplashViewController.swift
+++ coupang/SplashViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashViewController: UIViewController {
-
+    
+    @IBOutlet weak var appIcon: UIImageView!
+    @IBOutlet weak var lottieAnimationView: LottieAnimationView!
+    
+    // 메모리에 올라갔을 때 => 생성되고 초기화가 완료된 시점
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
-
+    
+    // 뷰가 화면에 보여진 직 후
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadViewIfNeeded()
+        UIView.animate(withDuration: 1,delay: 1.5) { [weak self] in
+                    self?.appIcon.alpha = 0
+                }
+        lottieAnimationView.play()
+    }
+    
+    
 }
