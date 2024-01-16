44ad3f5c527e6202f34530c93251086773bc90af
jeon yul <jeonyul.citylabs@gmail.com>
2024-01-16 11:17:17 +0900

    feat: splash

---
+++ coupang/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  coupang
+//
+//  Created by 전율 on 2024/01/16.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
